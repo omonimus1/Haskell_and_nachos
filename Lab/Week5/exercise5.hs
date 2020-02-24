0db3a402680ab8dae45e9b8f3539b0908a32ff2f
omonimus1 <davidepollicino2015@gmail.com>
2020-02-24 10:53:20 +0000

    Added solutions week 5 and week 6

--- Lab/Week5/exercise5.hs
+++ Lab/Week5/exercise5.hs
@@ -20,17 +20,41 @@ elemAt n (x:xs)
         | n == 1 = x
         | otherwise = elemAt (n-1) xs
 
-headSafe :: [a] -> Maybe a
-headSafe [] = Nothing
-headSafe (x:xs) = Just x
 
+findI' :: (Eq a) => a -> [a] -> Int
+findI' x [] = error "Not found"
+findI' x (y:ys)
+        | x == y = 1
+        | otherwise = 1 + findI' x ys
 
-
+findI :: (Eq a) => a -> [a] -> Maybe Int
+findI x ys
+        | elem x ys = Just (findI' x ys)
+        | otherwise = Nothing
 
 elemAtSafe :: Int -> [a] -> Maybe a
 elemAtSafe n xs
         | n > length xs || n <= 0 = Nothing
         | otherwise = Just (elemAt n xs)
+
+
+headSafe :: [a] -> Maybe a
+headSafe [] = Nothing
+headSafe (x:xs) = Just x
+
+lastSafe :: [a] -> Maybe a
+lastSafe [] = Nothing
+lastSafe xs = Just (last xs)
+
+safeLast :: [a] -> Maybe a
+safeLast [] = Nothing
+safeLast [x] = Just x
+safeLast (x:xs) = case (safeLast xs) of
+        Just n -> Just n
+        Nothing -> Nothing
+
+biggestPrime :: [Int] -> Maybe Int
+biggestPrime xs = lastSafe (filter isPrime xs)
 
 safeSum :: [Int] -> Maybe Int
 safeSum [] = Nothing
@@ -39,66 +63,49 @@ safeSum (x:xs) = case (safeSum xs) of
         Just n -> Just (n+x)
         Nothing -> Nothing
 
-
--- For lots of these questions, you will need to write helper functions.
-
-
-{-
-lastSafe :: [a] -> Maybe a
--}
-
-
-{-
-biggestPrime :: [Int] -> Maybe Int
--}
-
-
-{-
-safeMax :: (Ord a) => [a] -> Maybe a
--}
-
-
-{-
-biggestPrime2 :: [Int] -> Maybe Int
--}
-
-
-{-
-thereExistsWitness :: (a -> Bool) -> [a] -> Maybe a
--}
-
-
-{-
-forAllCounterexample :: (a -> Bool) -> [a] -> Maybe a
--}
-
-
-{-
-findIndex :: (Eq a) => a -> [a] -> Maybe Int
--}
-
-
-{-
-safeLast :: [a] -> Maybe a
--}
-
-
-{-
 removeLastSafe :: [a] -> Maybe [a]
--}
+removeLastSafe [] = Nothing
+removeLastSafe [x] = Just []
+removeLastSafe (x:xs) = case (removeLastSafe xs) of
+        Just ys -> Just (x:ys)
+        Nothing -> Nothing
 
+find' :: (a -> Bool) -> [a] -> a -- to be used when we know f x is true for some x <- xs
+find' f (x:xs)
+        | f x = x
+        | otherwise = find' f xs
 
+findFalse :: (a -> Bool) -> [a] -> a -- to be used when we know f x is false for some x <- xs
+findFalse f (x:xs)
+        | not (f x) = x
+        | otherwise = findFalse f xs
 
+thereExists :: (a -> Bool) -> [a] -> Bool -- any of the thereExists functions will work here.
+thereExists f xs = foldr (||) False (map f xs)
 
+thereExistsWitness :: (a -> Bool) -> [a] -> Maybe a
+thereExistsWitness f xs
+        | thereExists f xs = Just (find' f xs)
+        | otherwise = Nothing
 
+forAll :: (a -> Bool) -> [a] -> Bool -- any of the forAll functions will work here.
+forAll f xs = foldr (&&) True (map f xs)
 
+forAllCounterexample :: (a -> Bool) -> [a] -> Maybe a
+forAllCounterexample f xs
+        | forAll f xs = Nothing
+        | otherwise = Just (findFalse f xs)
 
+maxOfList' :: (Ord a) => [a] -> a -- must be used on non-empty list
+maxOfList' [x] = x
+maxOfList' (x:xs)
+        | x >= maxOfList' xs = x
+        | otherwise = maxOfList' xs
 
+safeMax :: (Ord a) => [a] -> Maybe a
+safeMax [] = Nothing
+safeMax xs = Just (maxOfList' xs)
 
-
-
-
-
-
-
+biggestPrime2 :: [Int] -> Maybe Int
+biggestPrime2 xs = safeMax (filter isPrime xs)
 
--- Lab/Week6/exercises6.hs
+++ Lab/Week6/exercises6.hs
@@ -1,12 +1,15 @@
-{- Use two clauses -}
--- myAnd :: Bool -> Bool -> Bool
+myAnd :: Bool -> Bool -> Bool
+myAnd False _ = False
+myAnd True x = x
 
-{- Use one clause -}
--- mySnd :: (a,b) -> a
+mySnd :: (a,b) -> a
+mySnd (_,y) = y
 
-{- Use four clauses -}
--- ignoreButN :: Int -> a -> a -> a -> Maybe a
-
+ignoreButN :: Int -> a -> a -> a -> Maybe a
+ignoreButN 1 x _ _ = Just x
+ignoreButN 2 _ y _ = Just y
+ignoreButN 3 _ _ z = Just z
+ignoreButN n _ _ _ = Nothing
 
 strange :: [Int] -> [Int]
 strange xs = map calculate xs
@@ -15,21 +18,22 @@ strange xs = map calculate xs
 strange2 :: [Int] -> [Int]
 strange2 xs = map (\x -> x^2 +4*x+1) xs
 
--- use where
--- absPlusOne :: [Int] -> [Int]
+absPlusOne :: [Int] -> [Int]
+absPlusOne xs = map absplus xs
+        where absplus x = abs x + 1
 
--- use an anonymous function
--- absPlusOne2 :: [Int] -> [Int]
+absPlusOne2 :: [Int] -> [Int]
+absPlusOne2 xs = map (\x -> abs x + 1) xs
 
--- use where
--- addPairs :: [(Int,Int)] -> [Int]
+addPairs :: [(Int,Int)] -> [Int]
+addPairs xs = map addPair xs
+        where addPair (x,y) = x+y
 
--- use an anonymous function
--- addPairs2 :: [(Int,Int)] -> [Int]
+addPairs2 :: [(Int,Int)] -> [Int]
+addPairs2 = map (\(x,y) -> x+y)
 
--- use an anonymous function
--- howMany2 :: (Eq a) => a -> [a] -> Int
-
+howMany2 :: (Eq a) => a -> [a] -> Int
+howMany2 x ys = foldl (\y z -> if x == z then y+1 else y) 0 ys
 
 quickSort :: (Ord a) => [a] -> [a]
 quickSort [] = []
@@ -37,5 +41,8 @@ quickSort (x:xs) = (lessThan xs) ++ [x] ++ (greaterThan xs)
         where lessThan xs = quickSort (filter (<x) xs)
               greaterThan xs = quickSort (filter (>=x) xs)
 
--- quickSort2 :: (Ord a) => [a] -> [a]
-
+quickSort2 :: (Ord a) => [a] -> [a]
+quickSort2 [] = []
+quickSort2 (x:xs) = (lessThan xs) ++ [x] ++ (greaterThan xs)
+        where lessThan xs = quickSort2 (filter (<x) xs)
+              greaterThan xs = quickSort2 (filter (>x) xs)
